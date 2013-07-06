c0e9c35469cefe396286776c7f92e888178d0c44
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-07-06 21:26:00 +0200

    added Sawa

---
+++ Sawa/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+
+import Control.Concurrent (threadDelay)
+import Control.Monad      (forever, void)
+import System.Process     (readProcess)
+
+
+main :: IO ()
+main = do
+    putStrLn "中"
+    doEveryMin 8 $ xte ["key Control_R"]
+
+
+xte :: [String] -> IO ()
+xte args = void $ readProcess "xte" args ""
+
+
+doEvery, doEverySec, doEveryMin :: Int -> IO () -> IO ()
+doEvery μ action = forever $ action >> threadDelay μ
+
+doEverySec s = doEvery (s * 10^6)
+doEveryMin m = doEverySec (m * 60)
