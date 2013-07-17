3a6cd99927d20ec82d249a03e282a06dea7fc5e7
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-07-17 19:06:05 +0200

    sound in Toki

--- Toki/Main.hs
+++ Toki/Main.hs
@@ -17,11 +17,17 @@ import System.Process       (readProcess)
 
 
 main :: IO ()
-main = callibrate $ everyQuarter display
+main = callibrate $ everyQuarter action
 -- main = display =<< getTime
+-- main = action "16:45" 3
+
+    where
+    action time n = do
+        forkIO $ display time
+        playSound n
 
 
-everyQuarter :: (String -> IO ()) -> IO ()
+everyQuarter :: (String -> Int -> IO ()) -> IO ()
 everyQuarter action = forever $ do
     time <- getCurrentTime
     zone <- getTimeZone time
@@ -37,9 +43,10 @@ everyQuarter action = forever $ do
     then main
     else do
         forkIO $ do
-            when (isQuarter minutes) .
+            when (isQuarter minutes) $
                 -- action $ formatTime defaultTimeLocale "%R" local
-                action $ formatTime defaultTimeLocale "%H時%M分" local
+                action (formatTime defaultTimeLocale "%H時%M分" local)
+                       (bells minutes)
 
             print minutes
 
@@ -48,6 +55,10 @@ everyQuarter action = forever $ do
     where
     isQuarter = (== 0) . (`mod` 15)
     outOfSync = (/= 0)
+    bells m   = let n = m `div` 15 in
+        if n == 0
+        then 4
+        else n
 
 
 callibrate :: IO () -> IO ()
@@ -64,15 +75,15 @@ callibrate action = do
         callibrate action
 
 
--- getTime :: IO String
--- getTime = do
---     time <- getCurrentTime
---     zone <- getTimeZone time
+getTime :: IO String
+getTime = do
+    time <- getCurrentTime
+    zone <- getTimeZone time
 
---     let local = utcToLocalTime zone time
+    let local = utcToLocalTime zone time
 
---     -- return $ formatTime defaultTimeLocale "%R" local
---     return $ formatTime defaultTimeLocale "%H時%M分" local
+    -- return $ formatTime defaultTimeLocale "%R" local
+    return $ formatTime defaultTimeLocale "%H時%M分" local
 
 
 threadDelaySec :: Integer -> IO ()
@@ -106,9 +117,16 @@ display time = do
         , Foreground  $ grey 255
         -- , Font        "Roboto-20:bold"
         , Font        "Ume Plus P Gothic-20"
+        -- , Font        "FZHei\\-B01-20"
         ]
 
         where
         height = 100
         width  = 200
         halve  = (`div` 2)
+
+
+playSound :: Int -> IO ()
+playSound n = void $ readProcess
+    "mplayer" [ "/usr/share/sounds/freedesktop/stereo/complete.oga"
+              , "-loop", show n ] ""
