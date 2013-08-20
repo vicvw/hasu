d6ee72e93450a9fb90464d4a9c3561e32becc309
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-08-20 18:45:33 +0200

    Volume: Reset with Omo; .compiles

--- Volume/Reset.hs
+++ Volume/Reset.hs
@@ -3,12 +3,18 @@ module Main where
 
 import Volume
 
+import Omo    (風, 空)
+import Omo.IO (differentIO)
+
 
 import Control.Monad  (replicateM_)
 
 
 main :: IO ()
-main = do
-    setVolumeOut 0
-
-    replicateM_ 2 increaseVolumeOut
+main = differentIO
+    $ 風 (do
+        setVolumeOut 0
+        replicateM_ 4 increaseVolumeOut)
+    . 空 (do
+        setVolumeOut 100
+        replicateM_ 6 decreaseVolumeOut)
