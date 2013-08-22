59ce88d424228047a3f11a08a08b5020a93dee60
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-08-23 00:10:02 +0200

    Omo: simplification

--- Omo/Omo.hs
+++ Omo/Omo.hs
@@ -1,6 +1,8 @@
 module Omo
     ( different
-    , 無, 全, 風, 空
+    , onHost
+    , 風, 空
+    , 全, 無
     , Diff
     , DiffT
     ) where
@@ -11,32 +13,65 @@ import Data.Maybe           (fromMaybe)
 
 
 different :: a -> (DiffT a) -> IO a
-different failed diff = (<$> hostname)
-    $ \host -> fromMaybe failed
-    . ($ diff empty)
-    $ case host of
-    "kaze" -> kaze
-    "sora" -> sora
+different def diff = (<$> hostname)
+    $ fromMaybe def
+    . (`lookup` diff [])
 
     where
     hostname = takeWhile (/= '\n')
            <$> readFile "/etc/hostname"
 
-    empty = 無 undefined
 
+風, 空 :: a -> DiffT a
+風 = onHost "kaze"
+空 = onHost "sora"
+
+全 :: a -> DiffT a
+全 x = foldl1 (.) . map ($ x) $ [風, 空]
 
 無 :: DiffT a
-無 = const $ Diff Nothing Nothing
+無 = const []
 
-全, 風, 空 :: a -> DiffT a
-全 x      = foldl1 (.) . map ($ x) $ [風, 空]
-風 x diff = diff { kaze = Just x }
-空 x diff = diff { sora = Just x }
+onHost :: String -> a -> DiffT a
+onHost host x = ((host, x) :)
 
 
-data Diff a = Diff
-    { kaze, sora :: Maybe a
-    } deriving (Show)
-
-
+type Diff  a = [(String, a)]
 type DiffT a = Diff a -> Diff a
+
+
+-- import Control.Applicative  ((<$>))
+-- import Data.Maybe           (fromMaybe)
+
+
+-- different :: a -> (DiffT a) -> IO a
+-- different failed diff = (<$> hostname)
+--     $ \host -> fromMaybe failed
+--     . ($ diff empty)
+--     $ case host of
+--     "kaze" -> kaze
+--     "sora" -> sora
+--     _      -> error "unknown host"
+
+--     where
+--     hostname = takeWhile (/= '\n')
+--            <$> readFile "/etc/hostname"
+
+--     empty = 無 undefined
+
+
+-- 無 :: DiffT a
+-- 無 = const $ Diff Nothing Nothing
+
+-- 全, 風, 空 :: a -> DiffT a
+-- 全 x      = foldl1 (.) . map ($ x) $ [風, 空]
+-- 風 x diff = diff { kaze = Just x }
+-- 空 x diff = diff { sora = Just x }
+
+
+-- data Diff a = Diff
+--     { kaze, sora :: Maybe a
+--     } deriving (Show)
+
+
+-- type DiffT a = Diff a -> Diff a
