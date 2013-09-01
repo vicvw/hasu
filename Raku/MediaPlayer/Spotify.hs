1a9fb622e8bf3e316b5a88a8cadb9b37ddc185d3
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-09-01 16:13:05 +0200

    Raku: added Spotify to MediaPlayer interface

---
+++ Raku/MediaPlayer/Spotify.hs
@@ -0,0 +1,47 @@
+module MediaPlayer.Spotify
+    ( spotify
+    ) where
+
+
+import MediaPlayer.Interface
+
+import qualified Spotify as S
+
+
+import Control.Applicative  ((<$>))
+import Control.Arrow        ((&&&))
+import Data.Maybe           (fromJust)
+
+
+spotify :: MediaPlayer
+spotify = MediaPlayer
+    { _isRunning  = S.isRunning
+
+    , _status     = showStatus . S._status <$> query
+    , _progress   = show
+                  . uncurry div
+                  . ((* 100) . S._position
+                     &&&
+                     S._length)
+                <$> query
+
+    , _artist     = const $ return ""
+    , _album      = const $ return ""
+    , _title      = \p -> (p ++)
+                        . S._title
+                      <$> query
+
+    , _play       = S.play
+    , _toggle     = S.toggle
+    , _stop       = S.stop
+    , _previous   = S.previous
+    , _next       = S.next
+    }
+
+    where
+    query = fromJust <$> S.query
+
+    showStatus status = case status of
+        S.Playing -> "再"
+        S.Paused  -> "休"
+        S.Stopped -> "止"
