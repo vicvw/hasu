b6ad81adf98db12cc4fedbdafd0ac0aa16b2c933
Victor Wiatr <victor.wiatr+github@gmail.com>
2013-12-22 03:12:21 +0100

    Cmus: fixed

--- Cmus/Cmus/Query.hs
+++ Cmus/Cmus/Query.hs
@@ -11,24 +11,24 @@ import Cmus.General
 
 import Text.ParserCombinators.Parsec
 
-import Control.Applicative  ((<$>), (<*), (*>))
+import Control.Applicative  ((<$>), (*>))
 
 
 query :: IO (Maybe Metadata)
 query = (<$> cmus Query) $ \mq -> (<$> mq) $ \q -> Metadata
     { _status           = toStatus   . parseQuery      q "status"                 $ many1 letter
-    , _file             =              parseQueryMaybe q "file"                   $ many1 anyChar
+    , _file             =              parseQueryMaybe q "file"                   $ manyNotNewline
     , _duration         = fmap toDur . parseQueryMaybe q "duration"               $ string "-1" <|> many1 digit
     , _position         = fmap read  . parseQueryMaybe q "position"               $ many1 digit
 
-    , _artist           =              parseQueryMaybe q "tag artist"             $ many anyChar
-    , _album            =              parseQueryMaybe q "tag album"              $ many anyChar
-    , _title            =              parseQueryMaybe q "tag title"              $ many anyChar
+    , _artist           =              parseQueryMaybe q "tag artist"             $ manyNotNewline
+    , _album            =              parseQueryMaybe q "tag album"              $ manyNotNewline
+    , _title            =              parseQueryMaybe q "tag title"              $ manyNotNewline
     , _discNumber       = fmap read  . parseQueryMaybe q "tag discNumber"         $ many digit
     , _trackNumber      = fmap read  . parseQueryMaybe q "tag trackNumber"        $ many digit
-    , _albumArtist      =              parseQueryMaybe q "tag albumArtist"        $ many anyChar
+    , _albumArtist      =              parseQueryMaybe q "tag albumArtist"        $ manyNotNewline
 
-    , _stream           =              parseQueryMaybe q "stream"                 $ many anyChar
+    , _stream           =              parseQueryMaybe q "stream"                 $ manyNotNewline
 
     , _aaaMode          = toAAAMode  . parseQuery      q "set aaa_mode"           $ many letter
     , _continue         = toBool     . parseQuery      q "set continue"           $ many letter
@@ -54,7 +54,8 @@ query = (<$> cmus Query) $ \mq -> (<$> mq) $ \q -> Metadata
         parser' = manyTill anyChar (try $ string str)
                *> space
                *> parser
-               <* newline
+
+    manyNotNewline = manyTill anyChar newline
 
     toStatus str = case str of
         "playing" -> Playing
