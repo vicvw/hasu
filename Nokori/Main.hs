b68855a00411719caf0eaa46d8dfbf3929be8d76
Victor Wiatr <victor.wiatr+github@gmail.com>
2015-04-24 00:17:12 +0200

    Nokori

---
+++ Nokori/Main.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE LambdaCase#-}
+
+module Main (main) where
+
+
+import Control.Applicative          ((<$>))
+import Control.Arrow                ((&&&), first)
+import Control.Monad                (void, when)
+
+import Data.Time.Calendar.WeekDate  (toWeekDate)
+import Data.Time.Clock              (addUTCTime, diffUTCTime, getCurrentTime, NominalDiffTime, UTCTime)
+import Data.Time.LocalTime          (getCurrentTimeZone, localDay, localTimeOfDay, todHour, todMin, utcToLocalTime, LocalTime)
+import Debug.Trace
+
+import System.Directory             (doesFileExist, removeFile)
+import System.Environment           (getArgs)
+import System.Process               (system)
+
+
+main :: IO ()
+main = getArgs >>= \case
+    ["s", h, m, s] -> do
+        now <- getCurrentTime
+
+        toFile
+            . (`addUTCTime` now)
+            . fromIntegral
+            . sum
+            . zipWith ($) [hours, mins, secs]
+            $ read <$> [h, m, s]
+
+    ["r"] -> removeFile file
+
+    ["m"] -> putStrLn =<< mode
+
+    ["t"] -> doesFileExist file >>= ii
+        (do until <- fromFile
+            now   <- getCurrentTime
+
+            let diff    = diffUTCTime until now
+                (h,m,s) = pretty diff
+                ifHour  = fi (h > 0)
+
+            when (diff < 0)
+                $ removeFile file
+               >> system `mapM_`
+                    -- [ "notify-send 殘"
+                    [ "exec mpv --no-terminal /usr/share/sounds/freedesktop/stereo/suspend-error.oga"
+                    ]
+
+            putStrLn $ concat
+                [ show0 h `ifHour` show0 m
+                , "時"    `ifHour` "分"
+                , " "
+                , show0 m `ifHour` show0 s
+                , "分"    `ifHour` "秒"
+                ])
+
+        ((=<< getLocalTime)
+            $ putStrLn
+            . (\(h,m) -> concat
+                [ show0 h
+                , "時"
+                , " "
+                , show0 m
+                , "分"
+                ])
+            . (todHour &&& todMin)
+            . localTimeOfDay)
+
+    _ -> error "fail"
+
+
+show0 :: Show a => a -> String
+show0 = (. show) $ \case
+    [x] -> '0' : [x]
+    x   -> x
+
+
+mode :: IO String
+mode = (=<< doesFileExist file) $
+    ii (return "殘") weekday
+
+
+weekday :: IO String
+weekday = (<$> getLocalTime)
+    $ 曜
+    . (\case (_,_,d) -> d)
+    . toWeekDate
+    . localDay
+
+    where
+    曜 = return . ("月火水木金土日" !!) . (subtract 1)
+
+
+getLocalTime :: IO LocalTime
+getLocalTime = do
+    z <- getCurrentTimeZone
+    t <- getCurrentTime
+    return $ utcToLocalTime z t
+
+
+pretty :: NominalDiffTime -> (Int, Int, Int)
+pretty dt = let t = max 0 $ floor dt
+    in let (h, r)  = t  `divMod` hours 1
+    in let (m, r') = r  `divMod` mins  1
+    in let (s, _)  = r' `divMod` secs  1
+    in (h, m, s)
+
+
+toFile :: UTCTime -> IO ()
+toFile = writeFile file . show
+
+fromFile :: IO UTCTime
+fromFile = read <$> readFile file
+
+
+file = "/home/v/.殘"
+
+
+hours, mins, secs :: Num a => a -> a
+hours = (* 60) . mins
+mins  = (* 60) . secs
+secs  = id
+
+
+fi :: Bool -> a -> a -> a
+fi = flip $ flip . ii
+
+ii :: a -> a -> Bool -> a
+ii t f = if' id (const t) (const f)
+
+if_ :: a -> (a -> Bool) -> (a -> b) -> (a -> b) -> b
+if_ x test t f = if' test t f x
+
+if' :: (a -> Bool) -> (a -> b) -> (a -> b) -> a -> b
+if' test t f x = ($ x) $ if test x then t else f
