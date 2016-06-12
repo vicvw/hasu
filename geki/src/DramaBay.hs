312023beae2dc2894e5a51fc23239cef384431b4
Victor Wiatr <victor.wiatr+github@gmail.com>
2016-06-13 01:01:00 +0200

    geki: stack

---
+++ geki/src/DramaBay.hs
@@ -0,0 +1,66 @@
+module DramaBay (spec) where
+
+
+import Control.Monad      (void)
+import Data.List          (isPrefixOf)
+
+import Text.HTML.TagSoup  ((~==), (~/=), fromTagText, isTagText, parseTags, partitions)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+-- import Text.HandsomeSoup  (css)
+-- import Text.XML.HXT.Core  (getText, hread, runLA, when, (>>>), (/>))
+-- import Text.XML.HXT.Core
+
+
+import Nara
+
+import Common
+
+
+spec = (url, episodes, links)
+
+
+url = "http://www.dramabay.com/new-episodes/"
+
+
+episodes :: Parser [Episode]
+episodes = do
+    name <- manyTill anyChar . try $ lookAhead (string "Special") <|> (string " E" <* notFollowedBy letter)
+    sp   <- lookAhead $ many1 anyChar
+
+    fi (sp == "Special")
+        (return [Episode DramaBay name 0 Nothing])
+        $ do
+            a    <- many1 digit
+            look <- lookAhead $ many anyChar
+
+            b    <- fi (null look || " (" `isPrefixOf` look)
+                        (return a)
+                        $ do
+                            void . manyTill anyChar $ char 'E'
+                            many1 digit
+
+            return $ (<$> [read a .. read b]) $ \n ->
+                Episode DramaBay name n Nothing
+
+
+links :: String -> [String]
+links
+    = tail
+    . filter (not . (`elem` "\n\r ") . head)
+    . map fromTagText
+    . filter isTagText
+    . takeWhile (~/= "</ul>")
+    . concat
+    . partitions (~== "<div id='col2'")
+    . parseTags
+
+
+-- links :: String -> [String]
+-- links html
+--     = traceShowId
+--     . ($ html)
+--     . runLA
+--     $ hread
+--     >>> css "#col2 a"
+--      /> getText
---
+++ geki/src/DramaNice.hs
@@ -0,0 +1,32 @@
+module DramaNice (spec) where
+
+
+import Data.List          (nub)
+
+import Text.HandsomeSoup  (css)
+import Text.XML.HXT.Core  (getText, hread, removeAllWhiteSpace, runLA, (>>>), (/>))--, (<+>))
+import Text.Megaparsec
+
+
+import Common
+
+
+spec :: Spec
+spec = Spec
+    { url = "http://dramanice.eu"
+
+    , parser = do
+        name <- manyTill anyChar $ string " Episode "
+        ep   <- some digitChar
+        return [Episode DramaNice name (read ep) Nothing]
+
+    , links = \html
+       -> nub
+        . ($ html)
+        . runLA
+        $ hread
+        >>> css "#recentadd" -- <+> css "#recentpopular"
+        >>> css ".sub .info-name a span"
+        >>> removeAllWhiteSpace
+         /> getText
+    }
